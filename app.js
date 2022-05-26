9e96d15b25c2b60e84fc06bcaf3344ee87ca3ef8
Rachel Kinzel <rharen11@su.edu>
2022-05-26 15:38:13 -0700

    add get guess fctn

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const game = {
     smallestNum: 1,
     secretNum: null,
     prevGuesses: [],
+    playerInput: null,
     play: function() {
       this.secretNum = Math.floor(Math.random() * 
         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
     },
     getGuess: function(){
       prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`)
+      while(playerInput <= 100){
+        this.playerInput = parseInt(this.playerInput)
+        return this.playerInput
+        if (this.playerInput <1) break
+      }
     }
   }
 
