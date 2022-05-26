f5d09147c01aecc419fade251acd5f9484658e40
Rachel Kinzel <rharen11@su.edu>
2022-05-26 15:07:01 -0700

    add prev guess and display prev fctn

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const game = {
     biggestNum: 100,
     smallestNum: 1,
     secretNum: null,
+    previousGuess: null,
     play: function() {
       this.secretNum = Math.floor(Math.random() * 
         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    }
+    displayPrevious: function(){
+      console.log("You're previous guess was ${this.previousGuess}")
     }
   }
