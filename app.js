c8694a9f24221e5616649e28a9bd5d65fec2a8a2
Rachel Kinzel <rharen11@su.edu>
2022-05-26 15:20:15 -0700

    add get guess fctn

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const game = {
     biggestNum: 100,
     smallestNum: 1,
     secretNum: null,
-    previousGuess: null,
+    prevGuesses: [],
     play: function() {
       this.secretNum = Math.floor(Math.random() * 
         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-    }
-    displayPrevious: function(){
-      console.log("You're previous guess was ${this.previousGuess}")
+    },
+    getGuess: function(){
+      prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`)
     }
   }
+
